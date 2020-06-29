867117473f1d547d4cbece943c948de04ef896fd
Lucia Brammer <brammerl96@gmail.com>
2020-06-29 12:01:09 -0700

    delete route for reviews and test

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -9,6 +9,7 @@ const Reviewer = require('../lib/models/Reviewer');
 const Review = require('../lib/models/Review');
 const Film = require('../lib/models/Film');
 const Studio = require('../lib/models/Studio');
+const chance = require('chance').Chance();
 
 describe('review routes', () => {
 
@@ -62,6 +63,75 @@ describe('review routes', () => {
           review: 'good film',
           film: film.id,
           __v: 0
+        });
+      });
+  });
+
+  it('gets top 100 reviews via GET', async() => {
+    reviewer = await Reviewer.create({
+      name: 'Breeann B',
+      company: 'Alchemy Code Lab'
+    });
+
+    studio = await Studio.create({
+      name: 'Portland Studio'
+    });
+
+    film = await Film.create({
+      title: 'film title',
+      studio: studio._id,
+      released: 2020
+    });
+
+    await Promise.all([... Array(150)].map(() => {
+      return Review.create({
+        rating: chance.pickone([1, 2, 3, 4, 5]),
+        reviewer: reviewer._id,
+        review: chance.paragraph({ sentences: 2 }),
+        film: film._id
+      });
+    })
+    );
+    return request(app)
+      .get('/api/v1/reviews/')
+      .then(res => {
+        expect(res.body).toHaveLength(100);
+      });
+  });
+
+  it('deletes review by ID via DELETE', async() => {
+    reviewer = await Reviewer.create({
+      name: 'Breeann B',
+      company: 'Alchemy Code Lab'
+    });
+
+    studio = await Studio.create({
+      name: 'Portland Studio'
+    });
+
+    film = await Film.create({
+      title: 'film title',
+      studio: studio._id,
+      released: 2020
+    });
+    
+    const review = await Review.create({
+      rating: chance.pickone([1, 2, 3, 4, 5]),
+      reviewer: reviewer._id,
+      review: chance.paragraph({ sentences: 2 }),
+      film: film._id
+    });
+
+    return request(app)
+      .delete(`/api/v1/reviews/${review._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          rating: review.rating,
+          reviewer: reviewer.id,
+          review: review.review,
+          film: film.id,
+          __v: 0,
         });
       });
   });
